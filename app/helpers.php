8d72c531483b8a8a78a1f2e5971f4721526ebdc6
SeBo <prog.sebo@gmail.com>
2025-03-06 19:04:52 +0100

    Create a template style (step 1)

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,15 @@
 <?php
+
 if (!function_exists('__')) {
     function __($key)
     {
         return \App\Core\Services\LanguageService::translate($key);
     }
 }
+
+if (!function_exists('isUserLoggedIn')) {
+    function isUserLoggedIn(): bool {
+        return isset($_SESSION['user']);
+    }
+}
+
